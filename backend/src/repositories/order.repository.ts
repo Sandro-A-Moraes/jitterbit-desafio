c098e05430fe4588c1816a46e3b3fb96e60433be
Sandro-A-Moraes <sandro.moraes3004@gmail.com>
2026-03-09 00:38:16 -0300

    feat: implement order repository functions for CRUD operations

--- backend/src/repositories/order.repository.ts
+++ backend/src/repositories/order.repository.ts
@@ -2,5 +2,60 @@ import {prisma} from '../lib/prisma'
 import type { OrderDB } from '../types/order-db';
 
 export async function createOrder(data: OrderDB) {
-    
+    return prisma.order.create({
+        data: {
+            orderId: data.orderId,
+            value: data.value,
+            creationDate: data.creationDate,
+            items: {
+                create: data.items.map(item => ({
+                    productId: item.productId,
+                    quantity: item.quantity,
+                    price: item.price
+                }))
+            }
+        },
+        include: {
+            items: true
+        }
+    })
 }
+
+export async function findOrderById(orderId: string) {
+    return prisma.order.findUnique({
+        where: { orderId },
+        include: {
+            items: true
+        }
+    })
+}
+
+export async function listOrders() {
+    return prisma.order.findMany({
+        include: {
+            items: true
+        }
+    })
+}
+
+export async function updateOrder(orderId: string, data: OrderDB) {
+    return prisma.order.update({
+       where: { orderId },
+       data: {
+           value: data.value,
+           creationDate: data.creationDate,
+           items: {
+               deleteMany: {},
+               create: data.items.map(item => ({
+                   productId: item.productId,
+                   quantity: item.quantity,
+                   price: item.price
+               }))
+           }
+       },
+       include: {
+           items: true
+       }
+    })
+}   
+   
