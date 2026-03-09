de989588a054e2b523c20decfa9c2da02214029a
Sandro-A-Moraes <sandro.moraes3004@gmail.com>
2026-03-09 01:08:13 -0300

    feat: refactor order routes for consistent formatting and improved readability

--- backend/src/routes/orderRoutes.ts
+++ backend/src/routes/orderRoutes.ts
@@ -1,16 +1,16 @@
-import {Router} from 'express'
-import * as orderController from '../controllers/order.controller'
+import { Router } from "express";
+import * as orderController from "../controllers/order.controller";
 
-const router = Router()
+const router = Router();
 
-router.post('/order', orderController.createOrder)
+router.post("/order", orderController.createOrder);
 
-router.get('/order/:id', orderController.getOrderById)
+router.get("/order/list", orderController.listOrders);
 
-router.get('/order/list', orderController.listOrders)
+router.get("/order/:id", orderController.getOrderById);
 
-router.put('/order/:id', orderController.updateOrder)
+router.put("/order/:id", orderController.updateOrder);
 
-router.delete('/order/:id', orderController.deleteOrder)
+router.delete("/order/:id", orderController.deleteOrder);
 
-export default router
+export default router;
